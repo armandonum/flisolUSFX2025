54de614e029432650bae35b6c0ac5623af1873bd
armando <armando@debian.armando>
2025-04-04 01:50:56 -0400

    template

---
+++ src/components/principal/principalFlisol.tsx
@@ -0,0 +1,15 @@
+import React,{ useState, useEffect} from 'react';
+
+const PrincipalFlisol = () => {
+  
+
+
+
+    return (
+        <div>
+            <h1>Flisol Principal</h1>
+          
+        </div>
+    );
+}
+export default PrincipalFlisol;
