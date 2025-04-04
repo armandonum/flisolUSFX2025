565e7091c3adafa1ec50bf56dde5cb301784596a
armando <armando@debian.armando>
2025-04-04 01:56:12 -0400

    template fix _V2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import PrincipalFlisol from './components/principal/principalFlisol'
 import Expositores from './components/expositores/expositores'
 import Organizadoresq from './components/organizadores/organizadores'
 import Colaboradores from './components/colaboradores/colaboradores'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       
