bbaaa598272a66c9998c5e10f58414d71706a7f7
armando <armando@debian.armando>
2025-04-12 14:03:26 -0400

    logo

--- src/components/colaboradores/colaboradores.tsx
+++ src/components/colaboradores/colaboradores.tsx
@@ -2,7 +2,6 @@ import "./colaboradores.css";
 import Colaborador from "./colaborador";
 import centro from "../../assets/img_colaboradores/centro.png";
 import fcyT from "../../assets/img_colaboradores/fcyT.png";
-import itgroup from "../../assets/img_colaboradores/itgroup.jpg";
 const colaboradores = () => {
   return (
     <>
